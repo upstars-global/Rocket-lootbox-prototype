2a4500bc26a85075bde9225605cfc9ac27950f56
Oleksandr Borysenko <kontynium@upstars.com>
2024-10-22 21:38:09 +0200

    add img

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,45 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="wrapper">
+    <img :src="baseLayer" class="rocket_base_layers" alt="" />
+    <img :src="liteLayer2" class="rocket_base_layers" alt="" /> 
+    <img :src="liteLayer4" class="rocket_base_layers" alt="" />
+    <img :src="liteLayer5" class="rocket_base_layers" alt="" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const baseLayer = ref('/src/img/Layer_1_for_all_rokets_720x1280.svg');
+const liteLayer2 = ref('/src/img/1-lite/Layer_2_1-Lite_720x1280.svg');
+const liteLayer4 = ref('/src/img/1-lite/Layer_4_1-Lite_720x1280.svg'); 
+const liteLayer5 = ref('/src/img/1-lite/Layer_5_1-Lite_720x1280.svg');
+</script>
+
+
+<style>
+.rocket_base_layers {
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translate(-50%);
+  height: 100%;
+  width: auto;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.wrapper {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+  overflow: hidden;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+body {
+  background: #000000;
+  margin: 0;
+  padding: 0;
+
 }
 </style>
